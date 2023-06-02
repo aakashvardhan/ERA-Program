53cf62e15f12eacbf4da7297be074a3a399df34d
aakashvardhan <amadabushi@hawk.iit.edu>
2023-06-02 17:09:36 -0500

    updated

--- S5 - Assignment/model.py
+++ S5 - Assignment/model.py
@@ -7,8 +7,11 @@ import torch.optim as optim
 class Net(nn.Module):
     #This defines the structure of the NN.
     def __init__(self):
+        ''' Initializer method where the layers of the NN are defined'''
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        '''2D conv layer that takes an input with 1 channel and outputs 32 channel.
+        The kernel size of the convolution is 3x3'''
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
@@ -17,6 +20,7 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = x.to("mps:0")
+        '''Defines the forward pass of the network. i.e, how 'x' is transformed into an output'''
         x = F.relu(self.conv1(x), 2)
         x = F.relu(F.max_pool2d(self.conv2(x), 2)) 
         x = F.relu(self.conv3(x), 2)
